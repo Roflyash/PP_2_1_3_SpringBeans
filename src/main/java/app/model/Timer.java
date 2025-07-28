261fd77960fbf62363e0056518e05f4c780ab024
Roflyash <oooheisenbergooo@gmail.com>
2025-07-28 10:10:37 +0400

    Задача PP_2_1_3_SpringBeans

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,15 @@
 package app.model;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+@Component
+@Scope("singleton")
 public class Timer {
+    private final Long nanoTime;
 
-    private Long nanoTime = System.nanoTime();
+    public Timer() {
+        this.nanoTime = System.nanoTime();
+    }
 
     public Long getTime() {
         return nanoTime;
